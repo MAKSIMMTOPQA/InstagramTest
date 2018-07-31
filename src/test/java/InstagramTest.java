1d05bebdbf245f952cbb28a940ffdd60fad7d852
Maksim Korshykov <maksim.korshikov00@gmail.com>
2018-07-31 18:44:22 +0300

    Instagram test

--- src/test/java/InstagramTest.java
+++ src/test/java/InstagramTest.java
@@ -1,3 +1,5 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
@@ -5,11 +7,19 @@ import static java.lang.Thread.sleep;
 public class InstagramTest extends InstagramBaseTest {
 
 
-    @Test
-    public void InstagramSuccessLikeTest() throws InterruptedException {
+    @DataProvider
+    public Object[][] validDataProvider() {
+        return new Object[][]{
+                { "iteatest@i.ua", "1q2w3e_4r5t" },
+
+        };
+    }
+
+    @Test (dataProvider="validDataProvider")
+    public void InstagramSuccessLikeTest (String email, String password) throws InterruptedException {
         InstagramSingInPage instagramSingInPage = instagramLoginPage.singIn();
         sleep (3000);
-        InstagramHomePage instagramHomePage = instagramSingInPage.enter();
+        InstagramHomePage instagramHomePage = instagramSingInPage.enter(email, password);
         sleep(2000);
 
         instagramHomePage.like();
@@ -18,7 +28,7 @@ public class InstagramTest extends InstagramBaseTest {
         sleep(2000);
         instagramAntonFoundPage.likeAndComment();
         sleep(2000);
-
+        Assert.assertEquals(instagramAntonFoundPage.getCurrentUrl("dfsdf" "fdsfsdf"));
         WriteCommentPage writeCommentPage = instagramAntonFoundPage.writenewcomment();
 
         writeCommentPage.writecommentToFriend();
